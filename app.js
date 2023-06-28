2a1e92c9ca599b9ea06ef531156aaa984eb3b01e
rafbar34 <rafbar34@gmail.com>
2023-06-28 23:23:22 +0200

    added connection with db

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const errorController = require('./controllers/error');
 
+const db = require('./util/database');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -13,7 +15,12 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+db.execute('SELECT * FROM products').then((result)=>{
+console.log(result)
+}).catch(err=>{
+    console.log(err)
+})
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
