f342de735a7a0f77d1325a09bfc75ef78d8bd175
rafbar34 <rafbar34@gmail.com>
2023-07-05 00:59:47 +0200

    feat add products into cart with qty

--- controllers/shop.js
+++ controllers/shop.js
@@ -54,13 +54,16 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user.getCart().then((cart) => {
-    return cart.getProducts().then(products=>{
-      res.render('shop/cart', {
-              path: '/cart',
-              pageTitle: 'Your cart',
-              products: products,
-            });
-    }).catch(err=>console.log(err));
+    return cart
+      .getProducts()
+      .then((products) => {
+        res.render('shop/cart', {
+          path: '/cart',
+          pageTitle: 'Your cart',
+          products: products,
+        });
+      })
+      .catch((err) => console.log(err));
   });
 
   // Cart.getCart((cart) => {
@@ -84,10 +87,36 @@ exports.getCart = (req, res, next) => {
 };
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId, (product) => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchedCart;
+  let newQuantity
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts({where: {id: prodId}});
+    })
+    .then((products) => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      newQuantity = 1;
+      if (product) {
+        const oldQuantity = product.CartItem.quantity
+        newQuantity = oldQuantity +1
+        return product
+      }
+
+      return Product.findByPk(prodId)
+    }).then(product =>{
+      return fetchedCart.addProduct(product, {
+        through:{quantity:newQuantity}
+      })
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch((err) => console.log(err));
 };
 exports.getCheckout = (req, res, next) => {
   Product.fetchAll((products) => {
